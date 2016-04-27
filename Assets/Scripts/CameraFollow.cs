1112b191451d7ee2f737e52bde995582e1b92132
Xephnos <orome1293@gmail.com>
2016-04-27 18:57:27 -0400

    New Camera Code

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Transform cameraTarget;
+    private Transform newCameraTarget;
+    private bool autoTarget;
+
+    void Awake()
+    {
+        newCameraTarget = GameObject.FindGameObjectWithTag("CameraTarget").transform;
+    }
+	
+	void Start ()
+    {
+        autoTarget = true;
+
+        if (autoTarget)
+        {
+            FindTarget();
+        }
+	}
+
+	void LateUpdate ()
+    {
+        if (autoTarget && (cameraTarget == null || !cameraTarget.gameObject.activeSelf))
+        {
+            FindTarget();
+        }
+        if (cameraTarget != null)
+        {
+            Follow(cameraTarget);
+        }
+	}
+
+    void FindTarget()
+    {
+        if (cameraTarget == null)
+        {
+            SetCameraTarget(newCameraTarget);
+        }
+    }
+
+    void Follow(Transform _target)
+    {
+        transform.position = Vector3.Lerp(transform.position, _target.transform.position, moveSpeed * Time.deltaTime);
+        transform.rotation = _target.rotation;
+    }
+
+    // PUBLIC METHODS
+    public void SetCameraTarget(Transform _target)
+    {
+        cameraTarget = _target;
+    }
+
+    public void SwitchTarget(Transform _target)
+    {
+        newCameraTarget = _target;
+        SetCameraTarget(newCameraTarget);
+    }
+}
