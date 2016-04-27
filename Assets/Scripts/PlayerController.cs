18967e6f0e0244d0e3ae6fe695c1c890ac467426
Xephnos <orome1293@gmail.com>
2016-04-27 18:19:52 -0400

    Movement, Rotation, & Jump + Grounded Check State

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float rotateSpeed;
+    public float jumpForce;
+    public float gravity;
+
+    private Vector3 moveDir;
+
+    private PlayerMotor myMotor;
+
+    void Awake()
+    {
+        myMotor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X");
+
+        if (myMotor.IsGrounded())
+        {
+            float horInput = Input.GetAxisRaw("Horizontal");
+            float vertInput = Input.GetAxisRaw("Vertical");
+
+            moveDir = new Vector3(horInput, 0, vertInput);
+            moveDir *= moveSpeed;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                moveDir.y = jumpForce;
+            }
+        }
+        else
+        {
+            moveDir.y += gravity * Time.deltaTime;
+        }
+       
+        moveDir = Vector3.ClampMagnitude(moveDir, moveSpeed);
+
+        myMotor.Rotate(new Vector3(0, mouseX * rotateSpeed, 0));
+        myMotor.Move(transform.rotation * moveDir);
+    }  
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMotor : MonoBehaviour
+{
+    public float smoothMovement;
+
+    private float distToGround;
+
+    private CharacterController charController;
+    private Collider myCollider;
+
+    void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+        myCollider = GetComponent<Collider>();
+    }
+
+    void Start()
+    {
+        distToGround = myCollider.bounds.extents.y;
+    }
+
+    public void Move(Vector3 movement)
+    {
+        charController.Move(movement * Time.deltaTime);
+    }
+
+    public void Rotate(Vector3 rotation)
+    {
+        transform.rotation *= Quaternion.EulerAngles(rotation * Time.deltaTime);
+    }
+
+    public void Jump()
+    {
+
+    }
+
+    public bool IsGrounded()
+    {
+        //return charController.isGrounded;
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+    }
+}
