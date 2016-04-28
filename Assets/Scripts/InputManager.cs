6a80dfc4f470016ffb9c10150d7cf06f56306984
Xephnos <orome1293@gmail.com>
2016-04-27 23:17:33 -0400

    Test

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class InputManager : MonoBehaviour {
+public class InputManager : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+	    
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	
 	}
 }
