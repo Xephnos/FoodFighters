467b0bedea7c6d990e89454a274de183cbffbdee
Xephnos <orome1293@gmail.com>
2016-05-03 14:56:13 -0400

    Keybindings Manager Finished! Saving and Loading Keybinds Implemented

--- Assets/Scripts/Game Managers/KeybindingsManager.cs
+++ Assets/Scripts/Game Managers/KeybindingsManager.cs
@@ -19,11 +19,11 @@ public class KeybindingsManager : MonoBehaviour
 
     void Start()
     {
-        keybindings.Add("Up", KeyCode.W);
-        keybindings.Add("Left", KeyCode.A);
-        keybindings.Add("Down", KeyCode.S);
-        keybindings.Add("Right", KeyCode.D);
-        keybindings.Add("Jump", KeyCode.Space);
+        keybindings.Add("Up", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Up", "W")));
+        keybindings.Add("Left", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Left", "A")));
+        keybindings.Add("Down", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Down", "S")));
+        keybindings.Add("Right", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Right", "D")));
+        keybindings.Add("Jump", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Jump", "Space")));
 
         up.text = keybindings["Up"].ToString();
         left.text = keybindings["Left"].ToString();
@@ -47,8 +47,33 @@ public class KeybindingsManager : MonoBehaviour
         }
     }
 
-    public void UpdateKeybind(GameObject _keybind)
+    public void UpdateKeybind(GameObject _clicked)
     {
-        keyPressed = _keybind;
+        keyPressed = _clicked;
+    }
+
+    public void SaveKeybindings()
+    {
+        foreach (var key in keybindings)
+        {
+            PlayerPrefs.SetString(key.Key, key.Value.ToString());
+        }
+
+        PlayerPrefs.Save();
+    }
+
+    public void ResetKeybindings()
+    {
+        keybindings["Up"] = KeyCode.W;
+        keybindings["Left"] = KeyCode.A;
+        keybindings["Down"] = KeyCode.S;
+        keybindings["Right"] = KeyCode.D;
+        keybindings["Jump"] = KeyCode.Space;
+
+        up.text = keybindings["Up"].ToString();
+        left.text = keybindings["Left"].ToString();
+        down.text = keybindings["Down"].ToString();
+        right.text = keybindings["Right"].ToString();
+        jump.text = keybindings["Jump"].ToString();
     }
 }
